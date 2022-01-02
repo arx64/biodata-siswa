f618391cc698cab34180e01075206061e1102e77
arx64 <33925496+arx64@users.noreply.github.com>
2022-01-03 02:31:17 +0700

    Delete daftar-ultah directory

--- daftar-ultah/admin/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "datasiswa-db";
-$dbc = mysqli_connect($hostname, $username, $password, $database) or die("Gagal Terhubung ke Database!");
-?>
--- daftar-ultah/admin/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo '<script>alert("Sukses Logout!");</script>';
-header("Location: index.php");
-?>
